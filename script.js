648ba3e0f116a6257e7ca071b0cf47d5b4218071
Boxieharp <97420558+Boxieharp@users.noreply.github.com>
2024-02-25 12:56:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 document.getElementById('pizza').addEventListener('click', function() {
   this.style.animation = 'spin 2s linear infinite';
-  this.style.transform = 'translate(-50%, -50%) scale(0.5, 0.5) rotate(360deg)';
 });
